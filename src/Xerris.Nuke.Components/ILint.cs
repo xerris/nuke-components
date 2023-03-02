53483dfe6d05207c72e84ece18860a1b7c3df101
Eric Sondergard <4650644+esond@users.noreply.github.com>
2023-03-02 11:26:11 -0600

    Add nuke build and implement a lint component

---
+++ src/Xerris.Nuke.Components/ILint.cs
@@ -0,0 +1,41 @@
+﻿using Nuke.Common;
+using Nuke.Common.ProjectModel;
+using Serilog;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+
+namespace Xerris.Nuke.Components;
+
+public interface ILint : ITools
+{
+    [Solution]
+    Solution Solution => TryGetValue(() => Solution);
+
+    [Parameter]
+    string? LintExclude => TryGetValue(() => LintExclude);
+
+    string ExcludedPathsArgument => string.IsNullOrEmpty(LintExclude)
+        ? $"--exclude {string.Join(' ', LintExclude)}"
+        : string.Empty;
+
+    Target Lint => _ => _
+        .DependsOn(RestoreTools)
+        .Executes(() =>
+        {
+            Log.Information($"Excluded paths: {string.Join(' ', LintExclude)}");
+
+            DotNet($"format whitespace {Solution} " +
+                   $"--verify-no-changes {ExcludedPathsArgument}");
+
+            DotNet($"format style {Solution} " +
+                   $"--verify-no-changes {ExcludedPathsArgument}");
+        });
+
+    Target FixLint => _ => _
+        .DependsOn(RestoreTools)
+        .Executes(() =>
+        {
+            DotNet($"format whitespace {Solution} {ExcludedPathsArgument}");
+
+            DotNet($"format style {Solution} {ExcludedPathsArgument}");
+        });
+}
---
+++ src/Xerris.Nuke.Components/ITools.cs
@@ -0,0 +1,13 @@
+﻿using Nuke.Common;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+
+namespace Xerris.Nuke.Components;
+
+public interface ITools : INukeBuild
+{
+    Target RestoreTools => _ => _
+        .Executes(() =>
+        {
+            DotNetToolRestore(_ => _);
+        });
+}
